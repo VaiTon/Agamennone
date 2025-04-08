067d470139fa93832eccf3bab70deb4327e0a7c4
VaiTon <eyadlorenzo@gmail.com>
2025-04-08 23:32:37 +0200

    Remove frontend (bye)

--- frontend/src/lib/api/config.ts
+++
@@ -1,5 +0,0 @@
-export const getConfig = async (server: string) => {
-	const res = await fetch(server + '/api/config');
-	const config = await res.json();
-	return config;
-};
--- frontend/src/routes/[serverAddress]/+page.ts
+++
@@ -1,8 +0,0 @@
-import type { PageLoad } from './$types';
-
-export const ssr = false;
-
-export const load = (({ params }) => {
-	const serverAddress = atob(params.serverAddress);
-	return { serverAddress };
-}) satisfies PageLoad;
