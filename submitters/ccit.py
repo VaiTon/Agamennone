bca4be30cd35ddde73e4c27e6a7a4bc4a7bec00c
VaiTon <eyadlorenzo@gmail.com>
2025-04-11 23:18:29 +0200

    Adapt config.json & fix CCIT submitter

--- submitters/ccit.py
+++ submitters/ccit.py
@@ -1,17 +1,30 @@
 #!/bin/env python3
 
-import sys, requests
+import sys
+import requests
 
 TEAM_TOKEN = "840ade564d40b2f82f11ef0d0087fda3"
 
 # read flags from stdin
 flags = [f.strip() for f in sys.stdin.readlines()]
-r = requests.put("http://10.10.0.1:8080/flags", headers={"X-Team-Token": TEAM_TOKEN}, json=flags)
-r = r.json()
+res = requests.put(
+    "http://10.10.0.1:8080/flags",
+    headers={"X-Team-Token": TEAM_TOKEN},
+    json=flags,
+).json()
 
-for obj in r:
-    status = "OK" if obj["status"] else "ERROR"
+# ORDER IS IMPORTANT
+# RULES ARE PROCESSED IN THE ORDER THEY ARE DEFINED
+RESULTS = {
+    "ACCEPTED": "OK",
+    "DENIED": "ERROR",
+    "RESUBMIT": "",  # empty string means the server will retry to submit the flag
+    "ERROR": "ERROR",
+}
+
+for obj in res:
     flag = obj["flag"]
     msg = obj["msg"]
+    status = RESULTS.get(obj["status"], "")
 
     print(flag, status, msg)
